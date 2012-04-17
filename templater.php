282bff6c20ac3e3af4ca63fe68da0571cabef3ab
Ashley Banks <ashleysmbanks89@gmail.com>
2012-04-17 21:52:02 +0100

    updated the templater file

--- templater.php
+++ templater.php
@@ -1,16 +1,18 @@
 <?php
 
 /**
-* Template Class
-*
-* Hoping to develop my own templating engine, just a simple one
-* this is the work in progress.
-
-* Hoping to have functionality of:
-*
-*  - Assinging a 'tagged' foreach loop in the view
-*  - Having conditional statements
-*  - Just rendering general tags
+ * Template Class
+ *
+ * Handles the templating, functionality atm:
+ *
+ * - Foreach Loops
+ * - Standard Tags Replacement
+ * - Conditional Statements - Boolean
+ *
+ * All based on [ ] tags
+ * 
+ * @author Ashley Banks
+ * Version: 1.2.1
 */
 
 class Templater {
@@ -18,6 +20,17 @@ class Templater {
 	private $_main_content,
 			$_template_tags;
 			
+	private static $_instance = NULL;
+	
+	public static function getInstance () 
+	{
+		if ( !self::$_instance instanceof self ) 
+			self::$_instance = new self();
+		
+		 return self::$_instance;
+	}	
+	
+	private function __Construct () {}
 
 	/**
 	 * Set up the content and return the rendered template.
@@ -25,15 +38,21 @@ class Templater {
 	 * @param string $template - the 'view' to render
 	 * @param assoc array $tags - the tags to replace
 	 */
-
-	public function set_content ( $template, $tags )
+	public function set_content ( $template, $tags, $use_header = TRUE, $use_footer = TRUE )
 	{
-		$this->_main_content = file_get_contents ( 'Views/' . $template . '.php' );
+		if ( $use_header )
+			$this->_main_content .= file_get_contents ( $_SERVER['DOCUMENT_ROOT'] . '/' . DIRECTORY . '/Views/overall_header.html' ); flush();
+		
+		$this->_main_content .= file_get_contents ( $_SERVER['DOCUMENT_ROOT'] . '/' . DIRECTORY . '/Views/' . $template . '.html' ); flush();
+		
+		if ( $use_footer )
+			$this->_main_content .= file_get_contents ( $_SERVER['DOCUMENT_ROOT'] . '/' . DIRECTORY . '/Views/overall_footer.html' ); flush();
+		
 		
 		$this->_template_tags = $tags;
 
-		$this->find_tags();
-
+		$this->find_tags ();
+		
 		$this->render ();
 	}
 
@@ -43,6 +62,7 @@ class Templater {
 	 */
 	private function find_tags ()
 	{
+		// Set the initial tags match
 		preg_match_all( "|\[(.*)\]|U", $this->_main_content, $matches );	
 
 		// Loops through the found tags
@@ -50,7 +70,7 @@ class Templater {
 		for( $i = 0; $i < count( $matches[ 1 ] ); $i++ )
 		{
 			// If a tag is a FOREACH we loop through the results by the given tag
-
+			
 			if ( substr ( $matches[ 1 ][ $i ], 0, 7 ) == 'FOREACH' )
 			{
 				// get the Array to loop
@@ -62,12 +82,19 @@ class Templater {
 
 				$this->_handle_foreach( $match[1], $match[3] );	
 			}
+			
+			elseif ( substr ( $matches[ 1 ][ $i ], 0, 2 ) == 'IF' )
+			{
+				$match = explode ( ' ', $matches[ 1 ][ $i ] );
+
+				$this->_conditional( $match[1] );	
+			}
 			else
 			{
 				// If its not a foreach tag or other stuff we can set the ordinary tags.
 				// Stops it from trying to replace and bloat shit that aint there.
 
-				if ( substr ( $matches[ 1 ][ $i ], 0, 7 ) !== 'FOREACH' || substr ( $matches[ 1 ][ $i ], 0, 2 ) !== '.' || substr ( $matches[ 1 ][ $i ], 0, 1 ) !== '/'  )
+				if ( substr ( $matches[ 1 ][ $i ], 0, 7 ) !== 'FOREACH' &&  substr ( $matches[ 1 ][ $i ], 0, 2 ) !== 'IF' && substr ( $matches[ 1 ][ $i ], 0, 2 ) !== '.' && substr ( $matches[ 1 ][ $i ], 0, 1 ) !== '/'  )
 				{
 					$this->set_tags ( $matches[1] );
 				}
@@ -77,7 +104,6 @@ class Templater {
 
 	/**
 	 * Swaps the tags for ordinary tags
-	 *
 	 * @param array $tags - the tags to replace.
 	 */
 
@@ -87,12 +113,21 @@ class Templater {
 		{	
 			foreach ( $tags as $tag )
 			{
-				$replace = $this->_template_tags[ $tag ];
-
-				if ( !!$replace )
-					$this->_main_content = str_replace ( '[' . $tag . ']', $replace, $this->_main_content );
+				$replace = $this->_template_tags[ strtolower ( $tag ) ];
+				
+				if ( !!$replace  )
+				{	
+					if ( $replace == ' ' )
+						$this->_main_content = str_replace ( '[' . $tag . ']', '', $this->_main_content );
+					elseif ( $replace == '' )
+						$this->_main_content = str_replace ( '[' . $tag . ']', '', $this->_main_content );
+					elseif ( preg_match("#\\[(.+)\\]#s", $replace ) === 1 )
+						$this->_main_content = str_replace ( '[' . $tag . ']', '', $this->_main_content );
+					else
+						$this->_main_content = str_replace ( '[' . $tag . ']', $replace, $this->_main_content );
+				}
 				else
-					$this->_main_content = str_replace ( '[' . $tag . ']', '', $this->_main_content );
+					continue;
 
 			}
 			return $this;
@@ -101,6 +136,30 @@ class Templater {
 		{
 			return FALSE;
 		}
+	}
+	
+	/**
+	 * Handles conditonal tags within the view.
+	 * This relies on TRUE / FALSE statements, so the conditions must be set in the PHP
+	 * @param string $c - the key in the tag that has been sent
+	 */
+	private function _conditional ( $c )
+	{
+		if ( !!$c )
+		{
+			$condition = $this->_template_tags[ strtolower ( $c ) ];
+			
+			preg_match ( '#\\[IF ' . $c . '](.+)\\[/ENDIF ' . $c . ']#s', $this->_main_content, $matches );
+			
+			if ( $condition )
+				$this->_main_content = preg_replace ( '#\\[IF ' . $c . '](.+)\\[/ENDIF ' . $c . ']#s', $matches[1], $this->_main_content );
+			else
+				$this->_main_content = preg_replace ( '#\\[IF ' . $c . '](.+)\\[/ENDIF ' . $c . ']#s', '', $this->_main_content );
+			
+			return $this;
+		}
+		else
+			return FALSE;	
 	}
 
 	/**
@@ -116,32 +175,36 @@ class Templater {
 	private function _handle_foreach ( $array, $array_tag )
 	{
 		// Get the array by the provided tags array
-
+		
 		$tag_array = $this->_template_tags[ $array ];
-
+		
 		// get everything between the foreach tags ( basically the tags to replace )
-
-		preg_match ( '#\\[FOREACH ' . $array . ' as ' . $array_tag . '](.+)\\[/FOREACH\\]#s', $this->_main_content, $matches2 );	
-
-		$mtch = trim ( strip_tags ( $matches2[1] ) );
+		
+		preg_match ( '#\\[FOREACH ' . $array . ' as ' . $array_tag . '](.+)\\[/' . $array . ' FOREACH\\]#s', $this->_main_content, $matches2 );	
+		
+		$mtch = trim ( $matches2[1] );
+		//$mtch = strip_tags ( $mtch );
 		$mtch = preg_replace( '/\s+/', ' ', $mtch );
+		$mtch = str_replace( '"', ' ', $mtch );
 		$mtch = explode ( ' ', $mtch );
-
+		
+		
 		// The content between the Foreach tags to loop through and build a string to output.
 
 		$_for_content = implode ( ',', $matches2 );
 		$_for_content = str_replace ( '[FOREACH ' . $array . ' as ' . $array_tag . ']', '', $_for_content );
-		$_for_content = str_replace ( '[/FOREACH]', '', $_for_content );
-		$_for_content = str_replace ( ' ', '', $_for_content);
+		$_for_content = str_replace ( '[/' . $array . ' FOREACH]', '', $_for_content );
+		//$_for_content = str_replace ( ' ', '', $_for_content);
 		$_for_content = trim ( $_for_content );
 		$_for_content = explode ( ',', $_for_content );
-
+		
+		
 		// Set loop content - this is the narrowed down tags from above.
 
 		$_loop_content = array();
 
 		// Push into array so we don't have aload of junk
-
+	
 		foreach ( $_for_content as $_c )
 		{
 			if ( !in_array ( $_c, $_loop_content ) )
@@ -151,9 +214,9 @@ class Templater {
 		// As its pushed in twice because of the regex we will take the second without the spacing.
 
 		$_loop_content = $_loop_content[1];
-
+		
 		// Loop the tag array for the foreach loop function
-
+	
 		foreach ( $tag_array as $row )
 		{
 			// Set default values - this is so they strings and array start again
@@ -163,81 +226,67 @@ class Templater {
 			$for_values = '';
 			$new_for_tags = array ();
 			$for_tags = '';
-
+			
 			// Loop through the items we want to replace and build up an array to 
 			// use for the string replace
-
+			
 			for ( $i = 0; $i < count( $mtch ); $i++ )
 			{
 				$item = $mtch[$i];
-				$item = explode ( '['.$array_tag, $item );
-				$item = str_replace ( array ( '.', ']' ), '', $item[1] );
-
-				// Build up the tags to replace
-
-				$for_tags .= '['.$array_tag.'.'.$item.'],';
-
-				// Build up the values to replace tags
-
-				$for_values .= $row[$item] . ',';
+				
+				// Check that what we are grabbing are actual tags in  [ ]
+				preg_match ( '#\\[' . $array_tag . '.(.+)\\]#s', $mtch[$i], $mtchhhh );
+			
+				$item = $mtchhhh[1];
+				
+				// If there is a match we go fourth, not to clog the array up with shit.
+				if ( !!$item )
+				{
+					// Set the for tags and the values to replace.
+					$item = str_replace( ']', '', $item ); // Had to replace this string because it was being a dick. Now I don't think I need the additional check
+					$for_tags .= '['.$array_tag.'.'.$item.'],';
+					$for_values .= $row[$item] . '^';
+				}
+					
 			}
-
 			// Explode the strings in array to pass through the str_replace
-
 			$for_tags = explode ( ',', $for_tags );
-			$for_values = explode ( ',', substr ( $for_values, 0, -1 ) );
-
+			$for_values = explode ( '^', substr ( $for_values, 0, -1 ) );
+			
 			foreach ( $for_tags as $t )
 			{
-				if ( !in_array ( $t, $new_for_tags )  )
+				if ( !in_array ( $t, $new_for_tags ) && !!$t )
 					array_push ( $new_for_tags, $t );
 			}
-
+			
 			// Build the content up
-
+			
 			$content .= str_replace( $new_for_tags, $for_values, $_loop_content );
-
+			
 			foreach ( $new_for_tags as $tg )
 			{
 				$this->_main_content = str_replace ( $tg, '', $this->_main_content );
 			}
+			
 		}
-
+		
 		// Set the content
 
-		$this->_main_content = preg_replace ( '#\\[FOREACH ' . $array . ' as ' . $array_tag . '](.+)\\[/FOREACH\\]#s', $content, $this->_main_content );
+		$this->_main_content = preg_replace ( '#\\[FOREACH ' . $array . ' as ' . $array_tag . '](.+)\\[/' . $array . ' FOREACH\\]#s', $content, $this->_main_content );
+		
 	}
 
 	/**
 	 * Display the template.
 	 */
+	 
 	private function render ()
 	{
 		echo $this->_main_content;	
 	}
+	
+	private function __clone () { }
 
 }
 
-
-	//////////////////////////////////////////////////////////////////////////////
-	// This is obviously just for test purposes
-	// Calling the class within the class file to save time moving about everywhere
-	// for the minute
-	//
-	$tmp = new Templater;
-	
-	$array = array ( 
-					  array ( 'title' => 'hellooooo', 'name' => 'Ashley Banks' ),
-					  array ( 'title' => 'gooodbbye', 'name' => 'Bradley Few' ),
-					  array ( 'title' => 'monkey', 'name' => 'asd' ),
-					  array ( 'title' => 'test', 'name' => 'Chicken man' )
-				   );
-				   
-	$tags['results'] = $array;
-	$tags['NAME'] = 'EVERYWHERE I GO I GOT CHEESE!';
-	
-	$tmp->set_content ( 'page', $tags );
-	
-	
-	//////////////////////////////////////////////////////////////////////////////
 ?>
